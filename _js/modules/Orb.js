b43b6fbeebd57e1caa606778527dd0ccb960209a
JorisHandstanger <joris.handstanger@hotmail.com>
2015-11-04 03:09:26 +0100

    First commit

---
+++ _js/modules/Orb.js
@@ -0,0 +1,116 @@
+'use strict';
+
+export default class Orb {
+
+  constructor(position, color){
+
+    this.color = color;
+    this.position = position;
+
+    this.obj = {};
+
+  }
+
+  update(controls){
+    this.obj.shape.children[1].material.uniforms.viewVector.value = new THREE.Vector3().subVectors(controls.getObject().position, this.obj.shape.children[2].position );
+    this.obj.shape.children[2].material.uniforms.viewVector.value = new THREE.Vector3().subVectors(controls.getObject().position, this.obj.shape.children[2].position );
+
+    var time = performance.now();
+    let sineScale = 1 - (Math.sin(time/800 - 1)/8);
+
+    this.obj.shape.children[0].intensity = sineScale*2;
+    this.obj.shape.children[1].scale.set(sineScale, sineScale, sineScale);
+    this.obj.shape.children[2].scale.set(sineScale, sineScale, sineScale);
+  }
+
+  render(camera){
+
+    let {x, y, z} = this.position;
+
+    var group = new THREE.Object3D();//create an empty container
+
+    var outerBall;
+    var middleBall;
+    var light1;
+
+    // Licht bal
+
+    var sphere = new THREE.SphereGeometry( 3, 30, 30 );
+
+    light1 = new THREE.PointLight( parseInt(this.color, 16), 3, 30 );
+    light1.add( new THREE.Mesh( sphere, new THREE.MeshBasicMaterial( { color: parseInt(this.LightenColor(this.color, 50), 16)})));
+
+    light1.position.x = x;
+    light1.position.y = y;
+    light1.position.z = z;
+
+    group.add(light1);//add a mesh with geometry to it
+
+    // Buitenste mesh
+
+    var geometry = new THREE.SphereGeometry(8, 30, 30);
+
+    var material = new THREE.ShaderMaterial({
+      uniforms: {
+        'c': {type: 'f', value: 1 },
+        'p': {type: 'f', value: 1 },
+        glowColor: { type: 'c', value: new THREE.Color(parseInt(this.color), 16)},
+        viewVector: { type: 'v3', value: camera.position }
+      },
+      vertexShader: document.getElementById('vertexShader').textContent,
+      fragmentShader: document.getElementById('fragmentShader').textContent,
+      side: THREE.FrontSide,
+      blending: THREE.AdditiveBlending,
+      transparent: true
+    });
+
+    outerBall = new THREE.Mesh(geometry.clone(), material.clone());
+    outerBall.position.x = x;
+    outerBall.position.y = y;
+    outerBall.position.z = z;
+
+    group.add(outerBall);
+
+    // Glow
+
+    geometry = new THREE.SphereGeometry(8, 60, 60);
+
+    material = new THREE.ShaderMaterial({
+      uniforms:
+      {
+        'c': {type: 'f', value: 0.1},
+        'p': {type: 'f', value: 6 },
+        glowColor: { type: 'c', value: new THREE.Color(parseInt(this.LightenColor(this.color, 50), 16)) },
+        viewVector: { type: 'v3', value: camera.position }
+      },
+      vertexShader: document.getElementById('vertexShader').textContent,
+      fragmentShader: document.getElementById('fragmentShader').textContent,
+      side: THREE.FrontSide,
+      blending: THREE.AdditiveBlending,
+      transparent: true
+    });
+
+    middleBall = new THREE.Mesh(geometry.clone(), material.clone());
+    middleBall.position.x = x;
+    middleBall.position.y = y;
+    middleBall.position.z = z;
+
+    group.add(middleBall);
+
+    this.obj.shape = group;
+
+    return this.obj.shape;
+
+  }
+
+  LightenColor(color, percent) {
+    var num = parseInt(color, 16),
+      amt = Math.round(2.55 * percent),
+      R = (num >> 16) + amt,
+      B = (num >> 8 & 0x00FF) + amt,
+      G = (num & 0x0000FF) + amt;
+
+    return (0x1000000 + (R<255?R<1?0:R:255)*0x10000 + (B<255?B<1?0:B:255)*0x100 + (G<255?G<1?0:G:255)).toString(16).slice(1);
+  }
+
+}
--- _js/script.js
+++ _js/script.js
@@ -7,8 +7,317 @@
 // or import specific polyfills
 // import {$} from './helpers/util';
 
-const init = () => {
-  console.log('Hello World Joris Handstanger');
-};
+import thecontrols from './modules/controls';
+import Orb from './modules/Orb';
+
+var camera, scene, renderer;
+var geometry, material, mesh;
+var controls;
+
+var orbs = [];
+
+var raycaster;
+
+var blocker = document.getElementById( 'blocker' );
+var instructions = document.getElementById( 'instructions' );
+
+// http://www.html5rocks.com/en/tutorials/pointerlock/intro/
+
+var havePointerLock = 'pointerLockElement' in document || 'mozPointerLockElement' in document || 'webkitPointerLockElement' in document;
+
+if ( havePointerLock ) {
+
+  var element = document.body;
+
+  var pointerlockchange = function () {
+
+    if ( document.pointerLockElement === element || document.mozPointerLockElement === element || document.webkitPointerLockElement === element ) {
+
+      controlsEnabled = true;
+      controls.enabled = true;
+
+      blocker.style.display = 'none';
+
+    } else {
+
+      controls.enabled = false;
+
+      blocker.style.display = '-webkit-box';
+      blocker.style.display = '-moz-box';
+      blocker.style.display = 'box';
+
+      instructions.style.display = '';
+
+    }
+
+  };
+
+  var pointerlockerror = function () {
+
+    instructions.style.display = '';
+
+  };
+
+  // Hook pointer lock state change events
+  document.addEventListener( 'pointerlockchange', pointerlockchange, false );
+  document.addEventListener( 'mozpointerlockchange', pointerlockchange, false );
+  document.addEventListener( 'webkitpointerlockchange', pointerlockchange, false );
+
+  document.addEventListener( 'pointerlockerror', pointerlockerror, false );
+  document.addEventListener( 'mozpointerlockerror', pointerlockerror, false );
+  document.addEventListener( 'webkitpointerlockerror', pointerlockerror, false );
+
+  instructions.addEventListener('click', function(){
+
+    instructions.style.display = 'none';
+
+    // Ask the browser to lock the pointer
+    element.requestPointerLock = element.requestPointerLock || element.mozRequestPointerLock || element.webkitRequestPointerLock;
+
+    if ( /Firefox/i.test( navigator.userAgent ) ) {
+
+      var fullscreenchange = function () {
+
+        if ( document.fullscreenElement === element || document.mozFullscreenElement === element || document.mozFullScreenElement === element ) {
+
+          document.removeEventListener( 'fullscreenchange', fullscreenchange );
+          document.removeEventListener( 'mozfullscreenchange', fullscreenchange );
+
+          element.requestPointerLock();
+        }
+
+      };
+
+      document.addEventListener( 'fullscreenchange', fullscreenchange, false );
+      document.addEventListener( 'mozfullscreenchange', fullscreenchange, false );
+
+      element.requestFullscreen = element.requestFullscreen || element.mozRequestFullscreen || element.mozRequestFullScreen || element.webkitRequestFullscreen;
+
+      element.requestFullscreen();
+
+    } else {
+
+      element.requestPointerLock();
+
+    }
+
+  }, false);
+
+} else {
+
+  instructions.innerHTML = 'Your browser doesn\'t seem to support Pointer Lock API';
+
+}
 
 init();
+animate();
+
+var controlsEnabled = false;
+
+var moveForward = false;
+var moveBackward = false;
+var moveLeft = false;
+var moveRight = false;
+
+var prevTime = performance.now();
+var velocity = new THREE.Vector3();
+
+function init() {
+
+  camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 1000 );
+
+  scene = new THREE.Scene();
+  scene.fog = new THREE.Fog( 0x000000, 0, 750 );
+
+  var light = new THREE.HemisphereLight( 0xeeeeff, 0x777788, 0.89 );
+  light.position.set( 0.5, 20, 0.75 );
+  scene.add( light );
+
+  controls = new THREE.PointerLockControls( camera );
+  scene.add( controls.getObject() );
+
+  for (var i = 0; i <= 10; i++) {
+    var orb = new Orb(
+      getRandomPoint(),
+      getRandomColor()
+    );
+    orbs.push(orb);
+  }
+
+  var onKeyDown = function ( event ) {
+
+    switch ( event.keyCode ) {
+
+    case 38: // up
+    case 90: // z
+      moveForward = true;
+      break;
+
+    case 37: // left
+    case 81: // q
+      moveLeft = true;
+      break;
+
+    case 40: // down
+    case 83: // s
+      moveBackward = true;
+      break;
+
+    case 39: // right
+    case 68: // d
+      moveRight = true;
+      break;
+
+    }
+
+  };
+
+  var onKeyUp = function ( event ) {
+
+    switch( event.keyCode ) {
+
+    case 38: // up
+    case 90: // z
+      moveForward = false;
+      break;
+
+    case 37: // left
+    case 81: // q
+      moveLeft = false;
+      break;
+
+    case 40: // down
+    case 83: // s
+      moveBackward = false;
+      break;
+
+    case 39: // right
+    case 68: // d
+      moveRight = false;
+      break;
+
+    }
+
+  };
+
+  document.addEventListener( 'keydown', onKeyDown, false );
+  document.addEventListener( 'keyup', onKeyUp, false );
+
+  raycaster = new THREE.Raycaster( new THREE.Vector3(), new THREE.Vector3( 0, -1, 0 ), 0, 10 );
+
+  // floor
+
+  geometry = new THREE.PlaneGeometry( 2000, 2000, 100, 100 );
+  geometry.rotateX(-Math.PI / 2 );
+
+  //vorm van vloer
+  for ( var i = 0, l = geometry.vertices.length; i < l; i++ ) {
+
+    var vertex = geometry.vertices[ i ];
+    vertex.x += Math.random() * 20 - 10;
+    vertex.y += Math.random() * 10;
+    vertex.z += Math.random() * 20 - 10;
+
+  }
+
+  material = new THREE.MeshPhongMaterial( { color: 0x444444, specular: 0x444444, shininess: 30 } );
+
+  mesh = new THREE.Mesh( geometry, material );
+  scene.add( mesh );
+
+  orbs.forEach(function(e) {
+    scene.add(e.render(camera));
+  });
+
+  //
+
+  renderer = new THREE.WebGLRenderer();
+  renderer.setClearColor( 0x000000 );
+  renderer.setPixelRatio( window.devicePixelRatio );
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  document.body.appendChild( renderer.domElement );
+
+  //
+
+  window.addEventListener( 'resize', onWindowResize, false );
+
+}
+
+function onWindowResize() {
+
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize( window.innerWidth, window.innerHeight );
+}
+
+function update(){
+  orbs.forEach(function(e) {
+    e.update(controls);
+  });
+}
+
+function animate() {
+
+  requestAnimationFrame( animate );
+
+  if ( controlsEnabled ) {
+    raycaster.ray.origin.copy( controls.getObject().position );
+    raycaster.ray.origin.y -= 10;
+
+    var time = performance.now();
+    var delta = ( time - prevTime ) / 1000;
+
+    velocity.x -= velocity.x * 5.0 * delta;
+    velocity.z -= velocity.z * 5.0 * delta;
+    velocity.y -= velocity.y * 5.0 * delta;
+
+    if ( moveForward ) velocity.z -= (500.0 * delta)-(((500.0 * delta)/(90 * Math.PI / 180))*Math.abs(controls.getPitchObject().rotation.x));
+    if ( moveBackward ) velocity.z += (500.0 * delta)-(((500.0 * delta)/(90 * Math.PI / 180))*Math.abs(controls.getPitchObject().rotation.x));
+
+    if ( moveLeft ) velocity.x -= 500.0 * delta;
+    if ( moveRight ) velocity.x += 500.0 * delta;
+
+    if ( moveForward ) velocity.y += ((500.0 * delta)/(90 * Math.PI / 180))*controls.getPitchObject().rotation.x;
+    if ( moveBackward ) velocity.y -= ((500.0 * delta)/(90 * Math.PI / 180))*controls.getPitchObject().rotation.x;
+
+
+    controls.getObject().translateX( velocity.x * delta );
+    controls.getObject().translateY( velocity.y * delta );
+    controls.getObject().translateZ( velocity.z * delta );
+
+    prevTime = time;
+
+  }
+
+  renderer.render( scene, camera );
+  update();
+
+}
+
+function getRandomPoint() {
+
+  return {
+    x: randomBetween(-300, 300),
+    y: randomBetween(10, 60),
+    z: randomBetween(-300, 300)
+  };
+
+}
+
+function randomBetween(min, max){
+
+  let rand = min + Math.random() * (max-min);
+  if(rand) rand = Math.round(rand);
+
+  return rand;
+}
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF'.split('');
+  var color = '0x';
+  for (var i = 0; i < 6; i++ ) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
